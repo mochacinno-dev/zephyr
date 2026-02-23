5611559be7c4dc9748100d8b12cf515557a8f832
Mochi <rblossom.dev@gmail.com>
2026-02-23 18:40:17 -0500

    Installer v0.0.1

---
+++ installer/Installer.java
@@ -0,0 +1,8 @@
+package installer;
+
+public class Installer {
+    public static void main(String[] args) {
+        System.out.println("ZEPHYR 1.0.0 Installer");
+        System.out.println("CLI VERSION");
+    }
+}
